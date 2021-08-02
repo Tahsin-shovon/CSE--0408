49bc85fd2e587a8dffb1ae0af25aeaf7e1905116
Tahsin Shovon <88005102+Tahsin-shovon@users.noreply.github.com>
2021-08-02 10:30:36 +0600

    Update 8_puzzle.cpp

--- class test/8_puzzle.cpp
+++ class test/8_puzzle.cpp
@@ -1,4 +1,4 @@
- #include<bits/stdc++.h> using namespace std; 
+#include<bits/stdc++.h> using namespace std; 
 #define D(x) cerr<<__LINE__<<" : "<<#x<<" -> "<<x<<endl 
 #define rep(i,j) for(int i = 0; i < 3; i++) for(int j = 0; j < 3; j++) 
 #define PII pair < int, int > typedef vector<vector<int>> vec2D; 
